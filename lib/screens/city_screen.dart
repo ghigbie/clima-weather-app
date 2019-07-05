fe93d2ffe4b14e3cd071901f08b76b5dc6a83c28
George Higbie <ghigbie@monigle.com>
2019-07-05 15:59:42 -0600

    Add constants

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -13,8 +13,40 @@ class _CityScreenState extends State<CityScreen> {
       body: Container(
         decoration:BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(),
+            image: AssetImage('images/city_background.jpg'),
             fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child:SafeArea(
+          child:Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child:FlatButton(
+                  onPressed: (){
+
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 50.0
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: null
+              ),
+              FlatButton(
+                onPressed: () {
+
+                },
+                child: Text(
+                  'Get Weather',
+                  style: kButtonTextStyle
+                )
+              )
+            ]
           )
         )
       )
