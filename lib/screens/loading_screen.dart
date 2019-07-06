ae4ddd960831da7b1e7d4433d0829b5f96ef0a81
George Higbie <ghigbie@monigle.com>
2019-07-06 16:05:41 -0600

    Working location weather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,33 +4,36 @@ import 'package:clima/services/location.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:clima/utilities/hidden_constants.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 String errorMessage = 'Getting your location...';
+double latitude;
+double longitude;
 
 void getLocation() async {
   Location location = Location();
   await location.getCurrentLocation();
-  print(location.latitute);
-  print(location.longitude);
+  latitude = location.latitute;
+  longitude = location.longitude;
+  getData();
 }
 
 void getData() async{
-  String baseURL = 'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22';
+  String baseURL = 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kAPIKey';
   http.Response response = await http.get(baseURL);
-
   if(response.statusCode == 200){
     String data = response.body;
     print('Respose: $data');
     var longitude = jsonDecode(data)['coord']['lon'];
     var latitude = jsonDecode(data)['coord']['lat'];
     var weatherDescription =jsonDecode(data)['weather'][0]['description'];
-    var temp =jsonDecode(data)['main']['temp'];
-    var id =jsonDecode(data)['weather'][0]['id'];
-    var city=jsonDecode(data)['name'];
+    var temp = jsonDecode(data)['main']['temp'];
+    var id = jsonDecode(data)['weather'][0]['id'];
+    var city =jsonDecode(data)['name'];
     print(longitude);
     print(latitude);
     print(weatherDescription);
@@ -41,8 +44,6 @@ void getData() async{
     print('Something went wrong: ');
     print(response.statusCode);
   }
-
-
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
@@ -55,7 +56,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Center(
         child: Text(errorMessage)
