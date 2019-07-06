47a1310084e7ace5af901631f61c200533040e89
George Higbie <ghigbie@monigle.com>
2019-07-06 16:50:07 -0600

    Add new dependency

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
-import 'package:clima/utilities/constants.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:clima/utilities/hidden_constants.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
-String errorMessage = 'Getting your location...';
-double latitude;
-double longitude;
-
-void getLocationData() async {
-  Location location = Location();
-  await location.getCurrentLocation();
-  latitude = location.latitute;
-  longitude = location.longitude;
-  NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kAPIKey');
-  var weatherData = await networkHelper.getData();
-}
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  String errorMessage = 'Getting your location...';
+  double latitude;
+  double longitude;
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitute;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$kAPIKey');
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) {
+        return LocationScreen();
+      }
+    ));
+  }
 
   @override 
   void initState(){
