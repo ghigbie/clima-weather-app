b4e903fba69390577e908e105b04c3aba6049886
George Higbie <ghigbie@monigle.com>
2019-07-06 22:41:13 -0600

    Add new inut text decoration

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -19,4 +19,20 @@ const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
 
+const kTextFieldInputDecoration = InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(
+                      Icons.location_city,
+                      color: Colors.white),
+                    hintText: 'Enter City Name',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide.none,
+                    ),
+                  );
+
 const kOpenWeatherBaseURL = 'https://api.openweathermap.org/data/2.5/weather';
